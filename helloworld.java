a1f5eede4e47963ae703b2fc11e5d5a359627f06
samplearjun <32512651+samplearjun@users.noreply.github.com>
2018-02-20 14:09:12 +0000

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Hello klghfghkf ");
+        System.out.println("Hello gjfgkfgkfj ");
  //creating object
   }
 }
