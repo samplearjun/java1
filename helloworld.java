cfed498357352e5fad1ef23733da9b75e755cc28
samplearjun <32512651+samplearjun@users.noreply.github.com>
2023-04-24 04:11:13 +0000

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,7 @@ public class helloworld
   public static void main(String[] args) {
         System.out.println("Hello World!, welcome to movate");
     
-        //System.out.println("New line added today ... 8th Nov 2021");
+        System.out.println("New line added today ... 8th Nov 2021");
  //creating object
   }
 }
